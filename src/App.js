159b517208b011001432905f3df40d5e2331e6d6
Ritik220519 <ritik220519@gmail.com>
2025-01-18 14:51:24 +0530

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     start Netflix-gpt App
+    </div>
+  );
+}
+
+export default App;
